771bf7390b49f9efb72670fa90387984e9fe1edd
Alvin Roe <dev@alvinroe.com>
2024-05-16 08:07:59 -0400

    Final commit before Kickstart.

--- lua/rash/remap.lua
+++ lua/rash/remap.lua
@@ -1,4 +1,4 @@
 vim.keymap.set('n', '<leader>pq', vim.cmd.Ex) 
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<C-c>', ':w !xclip -selection clipboard<CR><CR>',{ noremap = true, silent = true} )
-vim.keymap.set('v', '<C-c>', ':w !xclip -selection clipboard<CR><CR>',{ noremap = true, silent = true} )
+vim.keymap.set('v', '<C-c>', '"*y :call system("xclip -selection clipboard", @*)<CR>',{ noremap = true, silent = true} )
