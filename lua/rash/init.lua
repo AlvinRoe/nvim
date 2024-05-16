91b39826f060ffb3e7cd5234cdb4ce12aabd3e61
Alvin Roe <dev@alvinroe.com>
2024-05-16 11:01:46 -0400

    Kickstart customized.

---
+++ lua/rash/init.lua
@@ -0,0 +1,40 @@
+--Escape keybindings
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('v', 'v', '<Esc>')
+vim.keymap.set('x', 'v', '<Esc>')
+
+--Clibboard copy
+vim.keymap.set('n', '<C-c>', ':w !xclip -selection clipboard<CR><CR>')
+vim.keymap.set(
+	'v',
+	'<C-c>',
+	'"*y :call system("xclip -selection clipboard", @*)<CR>',
+	{ noremap = true, silent = true }
+)
+
+--Harpoon Keybindings
+vim.keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>")
+vim.keymap.set("n", "<leader>hs", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
+vim.keymap.set("n", "<leader>hn", ":lua require('harpoon.ui').nav_next()<CR>")
+vim.keymap.set("n", "<leader>hp", ":lua require('harpoon.ui').nav_prev()<CR>")
+vim.keymap.set("n", "<leader>hh", ":lua require('harpoon.ui').nav_file(1)<CR>")
+vim.keymap.set("n", "<leader>hj", ":lua require('harpoon.ui').nav_file(2)<CR>")
+vim.keymap.set("n", "<leader>hk", ":lua require('harpoon.ui').nav_file(3)<CR>")
+vim.keymap.set("n", "<leader>hl", ":lua require('harpoon.ui').nav_file(4)<CR>")
+vim.keymap.set("n", "<leader>h;", ":lua require('harpoon.ui').nav_file(5)<CR>")
+vim.keymap.set("n", "<leader>h1", ":lua require('harpoon.ui').nav_file(1)<CR>")
+vim.keymap.set("n", "<leader>h2", ":lua require('harpoon.ui').nav_file(2)<CR>")
+vim.keymap.set("n", "<leader>h3", ":lua require('harpoon.ui').nav_file(3)<CR>")
+vim.keymap.set("n", "<leader>h4", ":lua require('harpoon.ui').nav_file(4)<CR>")
+vim.keymap.set("n", "<leader>h5", ":lua require('harpoon.ui').nav_file(5)<CR>")
+vim.keymap.set("n", "<leader>h6", ":lua require('harpoon.ui').nav_file(6)<CR>")
+vim.keymap.set("n", "<leader>h7", ":lua require('harpoon.ui').nav_file(7)<CR>")
+vim.keymap.set("n", "<leader>h8", ":lua require('harpoon.ui').nav_file(8)<CR>")
+vim.keymap.set("n", "<leader>h9", ":lua require('harpoon.ui').nav_file(9)<CR>")
+
+--Background transparent
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
+--Configurations for vim ie. tab
+require('rash.set')
